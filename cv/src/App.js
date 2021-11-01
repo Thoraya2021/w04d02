fc75acba79737731af51f3bf24009c98be930e5a
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-01 16:59:30 +0300

    cv

---
+++ cv/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div className="part1">
+
+   <img src="https://clipground.com/images/white-profile-icon-clipart-2.png"></img>
+        <h1> Thoraya Alrashidi </h1>
+        <h2>Information Technology</h2>
+        <h4>Qassim ,Buraydah</h4>
+        <a href="thoraya.20gh@gmail.com">thoraya.20gh@gmail.com</a>
+     </div>
+
+    
+  );
+}
+
+export default App;
